594e7b3bc1ceea3abf5f77737d8d45a57410bd13
Majd Hamza <majd.hamza@worldline.com>
2025-07-23 16:48:06 +0200

    refactor: replace protocol with tlv

---
+++ internal/tlv/core.go
@@ -0,0 +1,87 @@
+package tlv
+
+import (
+	"ccache-backend-client/internal/constants"
+	"encoding/binary"
+)
+
+var (
+	SOCKET_PATH    string
+	FIXED_BUF_SIZE int
+	BACKEND_TYPE   string
+)
+
+type TLVField struct {
+	Tag    uint8
+	Length uint32
+	Data   []byte // Slice pointing to original buffer
+}
+
+type Message struct {
+	Type   uint16
+	Fields []TLVField
+}
+
+type Parser struct {
+	fields []TLVField // Reused slice to avoid allocations
+}
+
+type Serializer struct {
+	buffer []byte
+	pos    int
+}
+
+// encodeLength encodes a length using NDN variable-length encoding
+func encodeLength(buf []byte, length uint32) int {
+	if length <= uint32(constants.Length1ByteMax) {
+		buf[0] = uint8(length)
+		return 1
+	} else if length <= 0xFFFF {
+		buf[0] = constants.Length3ByteFlag
+		binary.BigEndian.PutUint16(buf[1:], uint16(length))
+		return 3
+	} else {
+		buf[0] = constants.Length5ByteFlag
+		binary.BigEndian.PutUint32(buf[1:], length)
+		return 5
+	}
+}
+
+// decodeLength decodes NDN variable-length encoding
+// Returns (length, bytesConsumed, error)
+func decodeLength(buf []byte) (uint32, int, error) {
+	if len(buf) < 1 {
+		return 0, 0, constants.ErrTruncatedData
+	}
+
+	firstByte := buf[0]
+
+	if firstByte <= constants.Length1ByteMax {
+		return uint32(firstByte), 1, nil
+	} else if firstByte == constants.Length3ByteFlag {
+		if len(buf) < 3 {
+			return 0, 0, constants.ErrTruncatedData
+		}
+		length := binary.BigEndian.Uint16(buf[1:3])
+		return uint32(length), 3, nil
+	} else if firstByte == constants.Length5ByteFlag {
+		if len(buf) < 5 {
+			return 0, 0, constants.ErrTruncatedData
+		}
+		length := binary.BigEndian.Uint32(buf[1:5])
+		return length, 5, nil
+	}
+
+	return 0, 0, constants.ErrInvalidLength
+}
+
+// lengthEncodingSize returns how many bytes are needed to encode a length
+func lengthEncodingSize(length uint32) int {
+	if length <= uint32(constants.Length1ByteMax) {
+		return 1
+	} else if length <= 0xFFFF {
+		return 3
+	} else {
+		return 5
+	}
+}
---
+++ internal/tlv/parser.go
@@ -0,0 +1,93 @@
+package tlv
+
+import (
+	"ccache-backend-client/internal/constants"
+	"encoding/binary"
+	"fmt"
+)
+
+func NewParser() *Parser {
+	return &Parser{
+		fields: make([]TLVField, 0, 16), // Pre-allocate for common case
+	}
+}
+
+// Parse parses a TLV message from the given buffer
+// Uses zero-copy approach - returned fields reference original buffer
+func (p *Parser) Parse(data []byte) (*Message, error) {
+	if len(data) < 1 {
+		return nil, constants.ErrInvalidMessage
+	}
+
+	p.fields = p.fields[:0]
+
+	// version := binary.LittleEndian.Uint16(data[0:2])
+	msgType := binary.LittleEndian.Uint16(data[2:4])
+	pos := 4
+
+	for pos < len(data) {
+		if pos+2 > len(data) {
+			return nil, constants.ErrTruncatedData
+		}
+
+		fieldType := data[pos]
+		pos += 1
+
+		length, lengthBytes, err := decodeLength(data[pos:])
+		if err != nil {
+			return nil, fmt.Errorf("failed to decode length: %w", err)
+		}
+		pos += lengthBytes
+
+		if uint64(pos)+uint64(length) > uint64(len(data)) {
+			return nil, constants.ErrTruncatedData
+		}
+
+		field := TLVField{
+			Tag:    fieldType,
+			Length: length,
+			Data:   data[pos : pos+int(length)], // Zero-copy
+		}
+
+		p.fields = append(p.fields, field)
+		pos += int(length)
+	}
+
+	return &Message{
+		Type:   msgType,
+		Fields: p.fields,
+	}, nil
+}
+
+// FindField finds the first field with the given type
+func (m *Message) FindField(fieldTag uint8) *TLVField {
+	for i := range m.Fields {
+		if m.Fields[i].Tag == fieldTag {
+			return &m.Fields[i]
+		}
+	}
+	return nil
+}
+
+// GetString extracts a string value from a field
+func (f *TLVField) GetString() string {
+	return string(f.Data)
+}
+
+// GetBool extracts a boolean value from a field
+func (f *TLVField) GetBool() bool {
+	return len(f.Data) > 0 && f.Data[0] != 0
+}
+
+// GetUint32 extracts a uint32 value from a field
+func (f *TLVField) GetUint32() uint32 {
+	if len(f.Data) < 4 {
+		return 0
+	}
+	return binary.LittleEndian.Uint32(f.Data)
+}
+
+// GetBytes returns the raw bytes of the field
+func (f *TLVField) GetBytes() []byte {
+	return f.Data
+}
