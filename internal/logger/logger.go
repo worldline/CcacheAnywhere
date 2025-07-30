3f9a1d6c19a6024b3230700f45ed7e45fbdc41b9
Majd Hamza <majd.hamza@worldline.com>
2025-07-30 11:04:40 +0200

    fix: correctly show the filename and line where the logger call happened

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"runtime"
 	"time"
 )
 
@@ -17,27 +19,44 @@ var (
 func OpenLogFile() error {
 	now := time.Now()
 	timestamp := now.Format("2006-01-02_15-04-05")
-	filename := fmt.Sprintf("%s_CLIENT_LOG", timestamp)
+	// /home/rocky/repos/py_server_script/daemons
+	filename := fmt.Sprintf("/home/rocky/repos/py_server_script/daemons/%s_CLIENT_LOG", timestamp)
 	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		return err
 	}
 
-	infoLogger = log.New(f, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	warningLogger = log.New(f, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
-	errorLogger = log.New(f, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	infoLogger = log.New(f, "INFO: ", log.Ldate|log.Ltime)
+	warningLogger = log.New(f, "WARNING: ", log.Ldate|log.Ltime)
+	errorLogger = log.New(f, "ERROR: ", log.Ldate|log.Ltime)
 	return nil
 }
 
 func LOG(v string, args ...any) {
 	if DEBUG_ENABLED {
-		infoLogger.Printf(v, args...)
+		_, file, line, ok := runtime.Caller(1)
+		if !ok {
+			infoLogger.Printf(v, args...)
+			return
+		}
+
+		filename := filepath.Base(file)
+		message := fmt.Sprintf(v, args...)
+		infoLogger.Printf("%s:%d: %s", filename, line, message)
 	}
 }
 
 func WARN(v string, args ...any) {
 	if DEBUG_ENABLED {
-		warningLogger.Printf(v, args...)
+		_, file, line, ok := runtime.Caller(1)
+		if !ok {
+			infoLogger.Printf(v, args...)
+			return
+		}
+
+		filename := filepath.Base(file)
+		message := fmt.Sprintf(v, args...)
+		warningLogger.Printf("%s:%d: %s", filename, line, message)
 	}
 }
 
